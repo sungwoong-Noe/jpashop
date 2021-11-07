b87a3524d9a7db7bc8dedf2c5b1a97b882401639
noseong-ung <asdz453@gmail.com>
2021-11-07 17:59:24 +0900

    Thymeleaf 테스트

--- src/main/java/jpabook/jpashop/Hello.java
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -1,4 +1,4 @@
-package jpabook;
+package jpabook.jpashop;
 
 import lombok.Getter;
 import lombok.Setter;
--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,6 +1,5 @@
 package jpabook.jpashop;
 
-import jpabook.Hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
